70754957b512a2d21420c965dc4f14debe11ec8a
Wyatt Armstrong <120539174+WyattArmstrongWA@users.noreply.github.com>
2025-01-20 02:41:44 -0600

    invalid input

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
-    //where is this going
+    print!("$ ");
+    io::stdout().flush().unwrap();
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    println!("{}: command not found", input.trim());
 }
